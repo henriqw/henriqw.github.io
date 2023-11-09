66d81ff12f4b20108fca32e3193be0ad3bfc6d28
henriqw <henriquewartchow@gmail.com>
2023-11-09 16:45:20 -0300

    up

---
+++ java/script.js
@@ -0,0 +1,8 @@
+let a = 0
+function mudar(){
+    document.getElementById("title").innerHTML = "Hello JavaScript!";
+    document.getElementById("one").style.fontSize = "25px";
+    document.getElementById("two").style.color = "red";
+    document.getElementById("two").style.backgroundColor = "yellow";
+    document.getElementById("image").src = "../midia/tv.gif";
+}
