e8ab892d8fa4c3318ca0143593b3788a97e28993
henriqw <henriquewartchow@gmail.com>
2023-11-22 21:11:34 -0300

    "

--- estudojs/script.js
+++ estudojs/script.js
@@ -1,3 +1,7 @@
-console.log ('Digite seu Nome')
-let nickname = Eu   
-console.log ('Bem-Vindo' + nickname)
+// pokemon_data
+let pokeSpecies = "Squirtle"
+let pokeName = "Whiplash!"
+let pokeLvl = 7
+let pokeHp = 12 
+let pokeGender = "F"
+let pokeIsAlive = true
--- java/script.js
+++ java/script.js
@@ -1,9 +1,9 @@
-let elList = document.querySelectorAll('.bro')
+let elList = document.querySelectorAll(".bro")
 
 function mudar(){
-    document.querySelector('#title').innerHTML = 'Hello JavaScript!';
-    elList.forEach(el => el.style.fontSize = '25px');
-    elList.forEach(el => el.style.backgroundColor = 'yellow');
-    elList.forEach(el => el.style.color = 'red');
-    document.getElementById('image').src = '../midia/tv.gif';
+    document.querySelector("#title").innerHTML = "Hello JavaScript!";
+    elList.forEach(el => el.style.fontSize = "25px");
+    elList.forEach(el => el.style.backgroundColor = "yellow");
+    elList.forEach(el => el.style.color = "red");
+    document.getElementById("image").src = "../midia/tv.gif";
 }
